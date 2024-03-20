3ad173795b71e54ef9ea60391f40532a2aa72b25
S573052 <S573052@nwmissouri.edu>
2024-03-20 14:49:42 -0500

    added computation

---
+++ routes/computation.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+let product = 1;
+let factor = 1;
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  console.log(req.query);
+
+  if (Object.keys(req.query).length === 0) {
+    console.log("Entered");
+    let r = 1 + Math.random() * 9;
+    res.render('computation', {
+      value1: `Math.fround() applied to ${r} is ${Math.fround(r)}`,
+      value2: `Math.random() applied to ${r} is ${Math.random(r)}`,
+      value3: `Math.round() applied to ${r} is ${Math.round(r)}`
+    });
+  } else {
+    for (let x in req.query) {
+      console.log(x);
+      res.render('computation', {
+        value1: `Math.fround() applied to ${req.query[x]} is ${Math.log2(req.query[x])}`,
+        value2: `Math.random() applied to ${req.query[x]} is ${Math.random(req.query[x])}`,
+        value3: `Math.round() applied to ${req.query[x]} is ${Math.round(req.query[x])}`
+      });
+    }
+  }
+});
+
+module.exports = router;
+
+
+
